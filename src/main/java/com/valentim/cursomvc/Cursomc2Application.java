bab21c5e566b01773b2896b53926d19fc2017dc9
eduardovalentim7 <eduardovalentim79@gmail.com>
2018-04-09 11:07:26 -0300

    Projeto Criado

---
+++ src/main/java/com/valentim/cursomvc/Cursomc2Application.java
@@ -0,0 +1,12 @@
+package com.valentim.cursomvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cursomc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cursomc2Application.class, args);
+	}
+}
